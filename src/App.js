5b81bbbec055265bc5e7e3b69678436a1dac16e8
Graham Cottridge <codinggc@gmail.com>
2019-12-10 23:53:49 +0000

    Set up home and navbar components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
-  );
+import NavBar from "../src/components/navbar/NavBar.component";
+import Home from "../src/pages/home/Home.component";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Route path="/" component={NavBar} />
+        <Route exact path="/" component={Home} />
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/navbar/NavBar.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./NavBar.styles.scss";
+
+class NavBar extends React.Component {
+  render() {
+    return <h1>NavBar</h1>;
+  }
+}
+
+export default NavBar;
