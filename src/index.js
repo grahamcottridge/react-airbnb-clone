c68bd30eb26aca85753a6a87c026b1438f94d75b
Graham Cottridge <codinggc@gmail.com>
2019-12-10 00:03:56 +0000

    Set up redux store

--- src/index.js
+++ src/index.js
@@ -3,4 +3,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "./redux/reducers/rootReducer";
+import reduxPromise from "redux-promise";
+
+const store = applyMiddleware(reduxPromise)(createStore)(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+
+  document.getElementById("root")
+);
