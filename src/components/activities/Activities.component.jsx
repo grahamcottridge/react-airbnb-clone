6c4a3c21ea81f0b7108a9ea47d5bf2c9a9693b20
Graham Cottridge <codinggc@gmail.com>
2020-01-14 23:46:01 +0000

    Set up activity and activities components

---
+++ src/components/activities/Activities.component.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Activity from "../activity/Activity.component";
+import "../activity/Activity.styles.scss";
+
+class Activities extends Component {
+  render() {
+    return (
+      <div className="activity">
+        <h1>Activity</h1>
+      </div>
+    );
+  }
+}
+
+export default Activities;
---
+++ src/components/activity/Activity.component.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./Activity.styles.scss";
+
+class Activity extends Component {
+  render() {
+    return (
+      <div className="activity">
+        <h1>Activities</h1>
+      </div>
+    );
+  }
+}
+
+export default Activity;
