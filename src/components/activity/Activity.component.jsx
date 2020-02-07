7ea04363a39d92ed54c5098aa40c2eab68740b43
Graham Cottridge <codinggc@gmail.com>
2020-02-07 23:54:20 +0000

    Set up activity component

--- src/components/activity/Activity.component.jsx
+++ src/components/activity/Activity.component.jsx
@@ -3,9 +3,25 @@ import "./Activity.styles.scss";
 
 class Activity extends Component {
   render() {
+    const {
+      activityType,
+      cost,
+      id,
+      image,
+      rating,
+      title,
+      totalRating
+    } = this.props.activity;
     return (
       <div className="activity">
-        <h1>Activities</h1>
+        <img src={image} />
+        <div className="activity-type">{activityType}</div>
+        <div className="title">{title}</div>
+        <div className="cost">From ${cost}/person</div>
+        <div className="rating">
+          <i className="material-icons">star</i>
+          {rating} ({totalRating})
+        </div>
       </div>
     );
   }
