6c3cbb7ad291936595ed4aee6ffc94ea7b813e78
Graham Cottridge <codinggc@gmail.com>
2020-02-10 23:25:03 +0000

    Add link to activities

--- src/components/activity/Activity.component.jsx
+++ src/components/activity/Activity.component.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./Activity.styles.scss";
+import { Link } from "react-router-dom";
 
 class Activity extends Component {
   render() {
@@ -14,14 +15,16 @@ class Activity extends Component {
     } = this.props.activity;
     return (
       <div className="activity">
-        <img src={image} />
-        <div className="activity-type">{activityType}</div>
-        <div className="title">{title}</div>
-        <div className="cost">From ${cost}/person</div>
-        <div className="rating">
-          <i className="material-icons">star</i>
-          {rating} ({totalRating})
-        </div>
+        <Link to={`/activity/${id}`}>
+          <img src={image} />
+          <div className="activity-type">{activityType}</div>
+          <div className="title">{title}</div>
+          <div className="cost">From ${cost}/person</div>
+          <div className="rating">
+            <i className="material-icons">star</i>
+            {rating} ({totalRating})
+          </div>
+        </Link>
       </div>
     );
   }
