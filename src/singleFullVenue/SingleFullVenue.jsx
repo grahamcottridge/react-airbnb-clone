3a590bf0a1dda069c993ca5de7ff8e194c8b1f31
Graham Cottridge <codinggc@gmail.com>
2020-02-20 23:50:36 +0000

    Set up SingleFullVenue page

---
+++ src/singleFullVenue/SingleFullVenue.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./SingleFullVenue.css";
+
+class SingleFullVenue extends Component {
+  render() {
+    return <h1>SingleFullVenue</h1>;
+  }
+}
+
+export default SingleFullVenue;
