6da4b90bff602d05fde1276d90291fcfa42e3f94
Graham Cottridge <codinggc@gmail.com>
2020-04-25 23:43:55 +0100

    Add modal to appropriate paths

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 import NavBar from "../src/components/navbar/NavBar.component";
 import Home from "../src/pages/home/Home.component";
 import SingleFullVenue from "./singleFullVenue/SingleFullVenue";
+import Modal from "./components/modal/modal.component";
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
         <Route path="/" component={NavBar} />
         <Route exact path="/" component={Home} />
         <Route exact path="/venue/:vid" component={SingleFullVenue} />
+        <Route path="/" component={Modal} />
       </Router>
     );
   }
